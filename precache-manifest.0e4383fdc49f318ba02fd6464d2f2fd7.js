7099b30a5395c077544192615a46329ae25432e7
buituan11 <buituan71099@gmail.com>
2019-11-01 12:40:13 +0700

    Updates

---
+++ precache-manifest.0e4383fdc49f318ba02fd6464d2f2fd7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1cb5b719973bef9ad55f6ef6387d02c0",
+    "url": "/hpbd/index.html"
+  },
+  {
+    "revision": "f24390d6a3e3b88e41ae",
+    "url": "/hpbd/static/css/main.f8b8e3f1.chunk.css"
+  },
+  {
+    "revision": "71a16e21f62b7d67ea5f",
+    "url": "/hpbd/static/js/2.90e94cbd.chunk.js"
+  },
+  {
+    "revision": "f24390d6a3e3b88e41ae",
+    "url": "/hpbd/static/js/main.a486d2e4.chunk.js"
+  },
+  {
+    "revision": "99637ce5f3264b37d902",
+    "url": "/hpbd/static/js/runtime~main.060bc1a3.js"
+  }
+]);
